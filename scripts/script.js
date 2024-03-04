cb3ade1973644b088b4581614b08ccf73bb5b96a
Malkevich Liliya E <MalkevichLE@bsu.by>
2024-03-04 17:11:06 +0300

    review

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    draggable: false
+  });
